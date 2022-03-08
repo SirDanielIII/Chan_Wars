c009dc2880e20fe7db0563e610feb66740a64c06
SirDaniel_daTurd <daniel1zhuo@gmail.com>
2022-03-08 14:45:07 -0500

    Update button class

--- bin/classes/button.py
+++ bin/classes/button.py
@@ -7,14 +7,18 @@ from ..tools.blit_tools import draw_text
 
 
 class Button(object):
-    def __init__(self, canvas, rect, clr, text_font, text_clr, stroke1=0, stroke2=0):
+    def __init__(self, canvas, x, y, w, h, clr, text, text_font, text_clr, stroke1=0, stroke2=0):
         self.canvas = canvas
-        self.rect = rect
+        self.x = x
+        self.y = y
+        self.rect = pg.Rect(x, y, w, h)
         self.rect_clr = clr
+        self.text = text
         self.text_font = text_font
         self.text_clr = text_clr
         self.rect_stroke = stroke1
         self.text_stroke = stroke2
+        self.overlay = pg.Surface((w, h), flags=pg.HWSURFACE and pg.DOUBLEBUF and pg.SRCALPHA).convert_alpha()
 
     def check_click(self, mx, my, click):
         """
@@ -30,17 +34,16 @@ class Button(object):
         Notes:
             Call this method before draw_button()
         """
-        if self.rect.collidepoint((mx, my)):
-            pg.draw.rect(self.canvas, (255, 255, 255, 50), self.rect)
-            if click:
-                return True
-            return False
+        if self.rect.collidepoint((mx, my)) and click:
+            return True
+        return False
 
-    def draw_button(self, text):
-        """
-        Args:
-            text::string:
-                Text to display
-        """
+    def draw_button(self, mx, my):
+        self.overlay.fill((0, 0, 0, 0))
         pg.draw.rect(self.canvas, self.rect_clr, self.rect)
-        draw_text(text, self.text_clr, self.text_font, self.canvas, self.rect.x + self.rect.w / 2, self.rect.y + self.rect.h / 2 - 2)
+        # Check for mouse hover
+        if self.rect.collidepoint((mx, my)):
+            self.overlay.fill((255, 255, 255, 50))
+        # Draw
+        self.canvas.blit(self.overlay, (self.x, self.y))
+        draw_text(self.text, self.text_clr, self.text_font, self.canvas, self.rect.x + self.rect.w / 2, self.rect.y + self.rect.h / 2 - 2)
--- bin/levels/main_menu.py
+++ bin/levels/main_menu.py
@@ -16,11 +16,12 @@ class MainMenu(Level):
         self.f_regular = pg.font.Font(os.getcwd() + "/bin/resources/Herculanum-Regular.ttf", 50)
         self.f_regular_big = pg.font.Font(os.getcwd() + "/bin/resources/Herculanum-Regular.ttf", 100)
         # Create Button Class
-        self.b_play_game = Button(self.text_canvas, pg.Rect(100, 400, 650, 150), blue, self.f_regular_big, white)
-        self.b_options = Button(self.text_canvas, pg.Rect(100, 600, 300, 100), blue, self.f_regular, white)
-        self.b_help = Button(self.text_canvas, pg.Rect(450, 600, 300, 100), blue, self.f_regular_small, white)
-        self.b_credits = Button(self.text_canvas, pg.Rect(100, 750, 300, 100), blue, self.f_regular, white)
-        self.b_quit = Button(self.text_canvas, pg.Rect(450, 750, 300, 100), blue, self.f_regular, white)
+        self.b_play_game = Button(self.text_canvas, 100, 400, 650, 150, blue, "Play Game", self.f_regular_big, white)
+        self.b_options = Button(self.text_canvas, 100, 600, 300, 100, blue, "Options", self.f_regular, white)
+        self.b_help = Button(self.text_canvas, 450, 600, 300, 100, blue, "How to Play", self.f_regular_small, white)
+        self.b_credits = Button(self.text_canvas, 100, 750, 300, 100, blue, "Credits", self.f_regular, white)
+        self.b_quit = Button(self.text_canvas, 450, 750, 300, 100, blue, "Quit", self.f_regular, white)
+        self.buttons = [self.b_play_game, self.b_options, self.b_help, self.b_credits, self.b_quit]
 
     def run(self):
         while True:
@@ -36,35 +37,18 @@ class MainMenu(Level):
                 if event.type == pg.QUIT or pressed[pg.K_ESCAPE]:
                     pg.quit()
                     sys.exit()
+                if event.type == pg.MOUSEBUTTONDOWN:  # When Mouse Button Clicked
+                    if event.button == 1:  # Left Mouse Button
+                        self.click = True
+
             # ------------------------------------------------------------------------------------------------------------------
             self.fill_screens()
-            # Play Game
-            if self.b_play_game.check_click(mx, my, self.click):
-                print("Work")
-
-            self.b_play_game.draw_button("Play Game")
             # ------------------------------------------------------------------------------------------------------------------
-            # Options
-            if self.b_options.check_click(mx, my, self.click):
-                print("Work")
-            self.b_options.draw_button("Options")
+            for i in self.buttons:
+                i.draw_button(mx, my)
+                if i.check_click(mx, my, self.click):
+                    print("done")
             # ------------------------------------------------------------------------------------------------------------------
-            # How to Play
-            if self.b_help.check_click(mx, my, self.click):
-                print("Work")
-            self.b_help.draw_button("How to Play")
-            # ------------------------------------------------------------------------------------------------------------------
-            # Credits
-            if self.b_credits.check_click(mx, my, self.click):
-                print("Work")
-            self.b_credits.draw_button("Credits")
-            # ------------------------------------------------------------------------------------------------------------------
-            # Quit Game
-            if self.b_quit.check_click(mx, my, self.click):
-                print("Work")
-            self.b_quit.draw_button("Quit")
-            # ------------------------------------------------------------------------------------------------------------------
-            # self.game_canvas.fill((255, 255, 255))
             self.blit_screens()
             self.clock.tick(self.FPS)
             pg.display.update()
