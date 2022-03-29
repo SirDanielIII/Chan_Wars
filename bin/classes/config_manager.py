ed57e69954de28930549a8b4668919963ba869de
SirDaniel_daTurd <daniel1zhuo@gmail.com>
2022-03-29 13:07:21 -0400

    Update Config File

--- bin/classes/config_manager.py
+++ bin/classes/config_manager.py
@@ -8,10 +8,12 @@ class Config(object):
     def __init__(self):
         with open(os.getcwd() + "/config.yml", "r") as f:
             self.data = yaml.load(f, Loader=yaml.FullLoader)
+            print(self.data)
 
     def return_boss_info(self, name):
         return self.data.get("bosses").get(name)
 
 # # Test
-# yeet = Config()
-# yeet.return_boss_info("mr_phone")
+yeet = Config()
+# # yeet.return_boss_info("mr_phone")
+# print(yeet.data.)
