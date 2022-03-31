207fa20373451a0944c0ae75fafd3248149924ab
SirDanielIII <daniel1zhuo@gmail.com>
2022-03-31 19:32:50 -0400

    Clean Up Boss Class

--- bin/classes/bosses.py
+++ bin/classes/bosses.py
@@ -5,45 +5,164 @@ from abc import ABC, abstractmethod
 
 
 class Boss(ABC):
-    def __init__(self, surface, configuration):
-        self.data = None
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def act(self):
+        pass
+
+    @abstractmethod
+    def update(self):
+        pass
+
+    @abstractmethod
+    def trigger(self):
+        pass
+
+    @abstractmethod
+    def death(self):
+        pass
+
+    @abstractmethod
+    def basic_action(self):
+        pass
+
+    @abstractmethod
+    def special_action(self):
+        pass
+
+
+class DevilChan(Boss):
+    def __init__(self, surface, config):
+        super().__init__()
         self.screen = surface
+        self.metadata = config["DevilChan"]
         self.trigger = None
-        self.config = configuration   
+        self.acting = True  # Whether or not it's the boss's turn
         self.time = 0
+        self.pos_mod = 50 * math.sin(self.time)
+        self.special = 0
+        self.health = self.metadata["hp"]
+        self.energy = self.metadata["energy"]
+        self.basic_power = self.metadata["basic"][1]
+        self.attack_phrases = self.metadata["phrases"]["basic"]
+
+    def act(self):
+        match self.trigger:
+            case "attack":
+                return self.trigger, self.basic_action()
+            case "die":
+                return self.trigger, self.death()
+            case "special":
+                return self.trigger, self.special_action()
+
+    def update(self, damage, time, boss_turn):
+        self.health -= damage
+        self.acting = boss_turn
+        self.time = time
+        self.energy = self.metadata["energy"]
         self.pos_mod = 50 * math.sin(time)
-        self.config = configuration
+        return self.health, self.energy, self.pos_mod
 
-    @abstractmethod
-    def update(self, damage):
-        pass
-
-    @abstractmethod
     def trigger(self):
-        pass
-
-    @abstractmethod
-    def special_action(self):
-        pass
+        if self.acting:
+            self.trigger = "attack"
+            if self.health < self.metadata["hp"] // 2 and not self.special:
+                self.trigger = "special"
+            if self.health <= 0:
+                self.trigger = "die"
 
     def death(self):
-        pass
+        return self.metadata["phrases"]["death"]
 
-    @abstractmethod
-    def update(self, damage):
-        pass
+    def basic_action(self):
+        return self.basic_power, self.attack_phrases[random.randint(0, len(self.attack_phrases) - 1)]
 
-    @abstractmethod
-    def trigger(self):
-        pass
-
-    @abstractmethod
     def special_action(self):
-        pass
+        self.health += self.metadata["special"][2]
+        self.special = 1
+        self.update(0, pg.time.get_ticks(), True)
+        return self.metadata["special"][1], self.metadata["phrases"]["special"][random.randint(0, len(self.metadata["phrases"]["special"]) - 1)]
 
-    @abstractmethod
+
+class MsG(Boss):
+    def __init__(self, surface, config):
+        super().__init__()
+        self.screen = surface
+        self.metadata = config["MsG"]
+        self.trigger = None
+        self.acting = True  # Whether or not it's the boss's turn
+        self.time = 0
+        self.pos_mod = 50 * math.sin(self.time)
+        self.special = 0
+        self.health = self.metadata["hp"]
+        self.energy = self.metadata["energy"]
+        self.basic_power = self.metadata["basic"][1]
+        self.attack_phrases = self.metadata["phrases"]["basic"]
+        self.siberia = False
+
+    def act(self):
+        match self.trigger:
+            case "attack":
+                return self.trigger, self.basic_action()
+            case "die":
+                return self.trigger, self.death()
+            case "special":
+                return self.trigger, self.special_action()
+
+    def update(self, damage, time, boss_turn):
+        self.health -= damage
+        self.acting = boss_turn
+        if self.siberia:
+            self.energy = self.metadata["energy"] - 1
+        else:
+            self.energy = self.metadata["energy"]
+        return self.health, self.energy, self.siberia
+
+    def trigger(self):
+        if self.acting:
+            self.trigger = "attack"
+            if self.health != self.metadata["hp"] and self.health > self.metadata["hp"] // 2:
+                self.special = 0
+            if (self.health == self.metadata["hp"] and not self.special) or (self.health <= self.metadata["hp"] // 2 and not self.special):
+                self.trigger = "special"
+            if self.health <= 0:
+                self.trigger = "die"
+
     def death(self):
-        pass
+        return self.metadata["phrases"]["death"]
+
+    def basic_action(self):
+        return self.basic_power, self.attack_phrases[random.randint(0, len(self.attack_phrases) - 1)]
+
+    def special_action(self):
+        if self.health > self.metadata["hp"] // 2:
+            self.special = 1
+            self.siberia = self.metadata["special"][1]
+            return self.siberia, self.metadata["phrases"]["special"][random.randint(0, len(self.metadata["phrases"]["special"]) - 1)]
+        else:
+            self.special = 1
+            self.siberia = False
+            return self.siberia, self.metadata["phrases"]["special"]["out"]
+
+
+class MrPhone(Boss):
+    def __init__(self, surface, config):
+        super().__init__()
+        self.screen = surface
+        self.metadata = config["DevilChan"]
+        self.trigger = None
+        self.acting = True  # Whether or not it's the boss's turn
+        self.time = 0
+        self.pos_mod = 50 * math.sin(self.time)
+        self.special = 0
+        self.health = self.metadata["hp"]
+        self.energy = self.metadata["energy"]
+        self.basic_power = self.metadata["basic"][1]
+        self.attack_phrases = self.metadata["phrases"]["basic"]
+        self.damaged = True
+        self.turn_count = 1
 
     def act(self):
         match self.trigger:
@@ -56,103 +175,6 @@ class Boss(ABC):
             case "kill":
                 return self.trigger, self.kill()
 
-    def basic_action(self):
-        return self.basic_power, self.attack_phrases[random.randint(0, len(self.attack_phrases) - 1)]
-
-
-
-class DevilChan(Boss):
-    def __init__(self, surface, configuration):
-        super().__init__(surface, configuration)
-        self.data = self.config["DevilChan"]
-        self.special = 0
-        self.health = self.data["hp"]
-        self.energy = self.data["energy"]
-        self.basic_power = self.data["basic"][1]
-        self.attack_phrases = self.data["phrases"]["basic"]
-
-    def update(self, damage, time, boss_turn):
-        self.health -= damage
-        self.energy = self.data["energy"]
-        self.acting = boss_turn
-        self.time = time
-        self.pos_mod = 50 * math.sin(time)
-        return self.health, self.energy, self.pos_mod
-
-    def trigger(self):
-        if self.acting:
-            self.trigger = "attack"
-            if self.health < self.data["hp"] // 2 and not self.special:
-                self.trigger = "special"
-            if self.health <= 0:
-                self.trigger = "die"
-
-    def special_action(self):
-        self.health += self.data["special"][2]
-        self.special = 1
-        self.update(0, pg.time.get_ticks(), True)
-        return self.data["special"][1], self.data["phrases"]["special"][random.randint(0, len(self.data["phrases"]["special"]) - 1)]
-
-    def death(self):
-        return self.data["phrases"]["death"]
-
-
-class MsG(Boss):
-    def __init__(self, surface, configuration):
-        super().init(surface, configuration)
-        self.data = self.config["MsG"]
-        self.special = 0
-        self.health = self.data["hp"]
-        self.energy = self.data["energy"]
-        self.siberia = False
-        self.basic_power = self.data["basic"][1]
-        self.attack_phrases = self.data["phrases"]["basic"]
-
-    def update(self, damage, time, boss_turn):
-        self.health -= damage
-        self.acting = boss_turn
-        if self.siberia:
-            self.energy = self.data["energy"] - 1
-        else:
-            self.energy = self.data["energy"]
-        return self.health, self.energy, self.siberia
-
-    def trigger(self):
-        if self.acting:
-            self.trigger = "attack"
-            if self.health != self.data["hp"] and self.health > self.data["hp"] // 2:
-                self.special = 0
-            if (self.health == self.data["hp"] and not self.special) or (self.health <= self.data["hp"] // 2 and not self.special):
-                self.trigger = "special"
-            if self.health <= 0:
-                self.trigger = "die"
-
-    def special_action(self):
-        if self.health > self.data["hp"] // 2:
-            self.special = 1
-            self.siberia = self.data["special"][1]
-            return self.siberia, self.data["phrases"]["special"][random.randint(0, len(self.data["phrases"]["special"]) - 1)]
-        else:
-            self.special = 1
-            self.siberia = False
-            return self.siberia, self.data["phrases"]["special"]["out"]
-
-    def death(self):
-        return self.data["phrases"]["death"]
-
-
-class MrPhone(Boss):
-    def __init__(self, surface, configuration):
-        super().init(surface, configuration)
-        self.data = self.config["MrPhone"]
-        self.special = 0
-        self.health = self.data["hp"]
-        self.energy = self.data["energy"]
-        self.basic_power = self.data["basic"][1]
-        self.attack_phrases = self.data["phrases"]["basic"]
-        self.damaged = True
-        self.turn_count = 1
-
     def update(self, damage, turn_counter, time, boss_turn):
         self.health -= damage
         self.acting = boss_turn
@@ -160,7 +182,7 @@ class MrPhone(Boss):
         if damage:
             self.damaged = True
         self.turn_count = turn_counter
-        self.energy = self.data["energy"]
+        self.energy = self.metadata["energy"]
         return self.health, self.energy
 
     def trigger(self):
@@ -173,11 +195,14 @@ class MrPhone(Boss):
             if self.health <= 0:
                 self.trigger = "die"
 
-    def special_action(self):
-        return self.data["special"][1], self.data["phrases"]["special"][random.randint(0, len(self.data["phrases"]["special"]) - 1)]
-
     def kill(self):
-        return self.data["kill"][1], self.data["phrases"]["kill"][random.randint(0, len(self.data["phrases"]["kill"]) - 1)]
+        return self.metadata["kill"][1], self.metadata["phrases"]["kill"][random.randint(0, len(self.metadata["phrases"]["kill"]) - 1)]
 
     def death(self):
-        return self.data["phrases"]["death"]
+        return self.metadata["phrases"]["death"]
+
+    def basic_action(self):
+        return self.basic_power, self.attack_phrases[random.randint(0, len(self.attack_phrases) - 1)]
+
+    def special_action(self):
+        return self.metadata["special"][1], self.metadata["phrases"]["special"][random.randint(0, len(self.metadata["phrases"]["special"]) - 1)]
